dc6ddacc4853fa01f568450f41ebe7bd1c8c96c9
Daniel Kiselev <kiselyovda@ya.ru>
2023-06-15 11:05:14 +0300

    decomposition

--- ofd/service/kkt.py
+++ ofd/service/kkt.py
@@ -1,33 +1,10 @@
-import typing as tp
-
-import httpx
-import pydantic as pd
-
-from .. import schemas
-
-
-def _get_data_from_request(url: str, params: dict,
-                           response_model: tp.Type[pd.BaseModel]) -> list[tp.Any]:
-    with httpx.Client() as client:
-        response = client.get(url, params=params)
-        match response.status_code:
-            case 200:
-                response_json: dict = response.json()
-                model = [
-                    response_model.parse_obj(items)
-                    for items in response_json.get('Data')
-                ]
-                return model
-            case 403:
-                raise httpx.HTTPError('403 Forbidden.')
-            case 503:
-                raise httpx.HTTPError('503. Server error')
+from .. import schemas, utils
 
 
 def request_list_kkt(auth_token: schemas.AuthToken) -> list[schemas.KktList]:
     url = 'https://ofd.ru/api/integration/v1/kkts'
     params = {'AuthToken': auth_token.auth_token}
-    kkt_list = _get_data_from_request(url, params, schemas.KktList)
+    kkt_list = utils.get_data_from_request(url, params, schemas.KktList)
     return kkt_list
 
 
@@ -35,7 +12,7 @@ def request_kkt_folders(auth_token: schemas.AuthToken, *,
                         group_id: str | None = None) -> list[schemas.Folder]:
     url = 'https://ofd.ru/api/integration/v1/kktgroup/list'
     params = {'AuthToken': auth_token.auth_token}
-    if id:
+    if group_id:
         params.update({'groupId': group_id})
-    folder = _get_data_from_request(url, params, schemas.Folder)
+    folder = utils.get_data_from_request(url, params, schemas.Folder)
     return folder
